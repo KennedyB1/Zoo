6909f57d4237792fae5d5ee32f1c622e34a0ebca
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-10-23 21:55:27 +0200

    Update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,19 @@ export const Home = () => {
 
 
     return (
-        <>Zoo</>
+        <>
+        <h1>Välkommen till ZooFeed! </h1>
+
+       <p> Hej och välkommen, engagerade djurskötare! ZooFeed är din ultimata plattform för att hålla koll på när våra älskade djur senast blev matade. På denna interaktiva hemsida har du möjlighet att delta i vårt dagliga vårdarbete genom att mata våra fyrbenta vänner och samtidigt säkerställa deras välbefinnande.</p>
+        
+       <h2> Hur fungerar det?</h2>
+        
+       <p>  Det är enkelt! När du matar ett djur, klickar du helt enkelt på knappen "matad" bredvid deras namn. Genom att göra detta hjälper du oss att hålla reda på deras matvanor och se till att varje djur får den omsorg och näring de behöver.</p>
+        
+        <h2>  Varför är det viktigt?</h2>
+        
+        <p>  Genom att mata djuren regelbundet säkerställer vi inte bara deras fysiska hälsa, utan vi skapar också en starkare och djupare relation med dem. Ditt deltagande spelar en viktig roll i vår strävan att skapa en trygg och kärleksfull miljö för alla våra invånare.
+        
+        Så känn dig som hemma, interagera med djuren och hjälp oss att göra vårt zoo till en bättre plats för alla varelser som bor här. Tack för ditt engagemang och ditt bidrag till vårt gemensamma mål att främja djurens välmående!</p></>
     );
 };
